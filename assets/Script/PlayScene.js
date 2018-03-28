eea86d4804f595e1e619f862ba4ca4d2551fdbc5
Sang Li <sang.lxuan@gmail.com>
2018-03-28 17:43:17 +0700

    add play scene

---
+++ assets/Script/PlayScene.js
@@ -0,0 +1,21 @@
+
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        answer_value: [cc.Label]
+    },
+
+    onTouchItem: function(event, data) {
+        cc.log("event:", event);
+        cc.log("data:", data);
+    },
+
+
+    start:function() {
+
+    }
+
+    // update (dt) {},
+});
